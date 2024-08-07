005815331d55e8379cc307bfb1170c432a634070
Andrew Schweitzer <schweitz72@gmail.com>
2024-08-07 11:00:44 -0400

    Add ruff; remove black, flake8, isort

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 # type: ignore
 
 """Nox sessions."""
+
 import os
 import shlex
 import shutil
@@ -10,10 +11,8 @@ from textwrap import dedent
 
 import nox  # noqa
 
-
 try:
-    from nox_poetry import Session
-    from nox_poetry import session
+    from nox_poetry import Session, session
 except ImportError:
     message = f"""\
     Nox failed to import the 'nox-poetry' package.
@@ -120,14 +119,7 @@ def precommit(session: Session) -> None:
         "--show-diff-on-failure",
     ]
     session.install(
-        "black",
-        # "darglint",
-        # "flake8",
-        # "flake8-bandit",
-        # "flake8-bugbear",
-        # "flake8-docstrings",
-        # "flake8-rst-docstrings",
-        "isort",
+        "ruff",
         "pep8-naming",
         "pre-commit",
         "pre-commit-hooks",
